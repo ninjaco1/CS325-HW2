5691b6200a237cdb5d251be60dbcd4694406ebc7
Anthony Nguyen <nguyean6@flip3.engr.oregonstate.edu>
2020-01-22 22:29:36 -0800

    final

--- merge3Time.py
+++ merge3Time.py
@@ -5,13 +5,43 @@ import time
 
 
 def main():
-    array = []
-    for i in range(10):
-        array.append(randint(0,100))
-    print (array)
-    mergesort3(array)
-    print(array)
+    run_time = []
+    for i in range(50000,550000,50000):
+        array = []
+        sum = 0
+        temp_runtime =[]
+        for j in range(i):
+            array.append(randint(0,10000))
+        for k in range(3): # run 3 times then average
+            temp = array.copy()
+            #print(array)
+            start_time = time.time()
+            mergesort3(temp)
+            end_time = time.time() - start_time
+            temp_runtime.append(end_time)
+            #print(end_time)
+            sum += end_time
+            #temp.clear()
 
+        temp_runtime.append(sum/3)
+        print(temp_runtime)
+        string = ""
+        for number in temp_runtime:
+            string += str(number) + " "
+        string += "\n"
+        #print(string) 
+        run_time.append(string)
+        array.clear()
+        temp_runtime.clear()
+    #print(run_time)
+    runtime_file(run_time)
+
+def runtime_file(runtime):
+    file = open("merge3time.txt","w")
+    file.truncate(0)
+    for string in runtime:
+        file.write(string)
+    file.close()
 
 
 def mergesort3(array):
@@ -30,56 +60,74 @@ def mergesort3(array):
         mergesort3(bot)
         mergesort3(mid)
         mergesort3(up)
-        #merge(bot,mid,up)
-        #conquer
-        i = j = k = z = 0 # bot, mid, up, main array
-        #comparing all to see what is the smallest between them
-        while i < len(bot) and j < len(mid) and k < len(up):
-            if bot[i] < mid[j]:
-                if bot[i] < up[k]:
-                    array[z] = bot[i]
-                    i+=1
-                else:
-                    array[z] = up[k]
-                    k+=1
-                z+=1
-            else: # if mid is smaller or up is smaller
-                if mid[j] < up[k]:
-                    array[z] = mid[j]
-                    j+=1
-                else:
-                    array[z] = up[k]
-                    k+=1
-                z+=1
+        merge(array,bot,mid,up)
 
-        # just like mergesort2, merge 1 and 2, 2 and 3, and 1 and 3
-        while i < len(bot) and j < len(mid):
-            if bot[i] < mid[j]:
+
+def merge(array,bot,mid,up):
+    #conquer
+    i = j = k = z = 0 # bot, mid, up, main array
+    #comparing all to see what is the smallest between them
+    while i < len(bot) and j < len(mid) and k < len(up):
+        if bot[i] < mid[j]:
+            if bot[i] < up[k]:
                 array[z] = bot[i]
                 i+=1
             else:
+                array[z] = up[k]
+                k+=1
+            z+=1
+        else: # if mid is smaller or up is smaller
+            if mid[j] < up[k]:
                 array[z] = mid[j]
                 j+=1
+            else:
+                array[z] = up[k]
+                k+=1
             z+=1
-        while j < len(mid) and k < len(up):
-        while i < len(bot) and k < len(up)
 
-
-        #add the rest of the list back up
-        while i < len(bot):
+    # just like mergesort2, merge 1 and 2, 2 and 3, and 1 and 3
+    while i < len(bot) and j < len(mid):
+        if bot[i] < mid[j]:
             array[z] = bot[i]
-            i += 1
-            z += 1
-
-        while j < len(mid):
+            i+=1
+        else:
             array[z] = mid[j]
-            j += 1
-            z += 1
-
-        while k < len(up):
+            j+=1
+        z+=1
+    while j < len(mid) and k < len(up):
+        if mid[j] < up[k]:
+            array[z] = mid[j]
+            j+=1
+        else:
             array[z] = up[k]
-            k += 1
-            z += 1
+            k+=1
+        z+=1
+    while i < len(bot) and k < len(up):
+        if bot[i] < up[k]:
+            array[z] = bot[i]
+            i+=1
+        else:
+            array[z] = up[k]
+            k+=1
+        z+=1
+
+
+    #add the rest of the list back up
+    while i < len(bot):
+        array[z] = bot[i]
+        i += 1
+        z += 1
+
+    while j < len(mid):
+        array[z] = mid[j]
+        j += 1
+        z += 1
+
+    while k < len(up):
+        array[z] = up[k]
+        k += 1
+        z += 1
+
 
 
 
---
+++ mergesort3.py
@@ -0,0 +1,125 @@
+import random
+import numpy as np
+import time
+import os
+
+def main():
+    data = open("data.txt", "r")
+    line = data.readlines()
+    data.close()
+    #print(line)
+    array = []
+    # loop through each line and sort and append each line into array
+    for j in range(len(line)):
+        temp = [int(i) for i in line[j].split() if i.isdigit()]
+        mergesort3(temp)
+        array.append(temp)
+
+    #print(array)
+
+    #insert sorted data value in a different file
+    out_file(array)
+
+    # array = [1,5,2,6,9,0,8,3,7]
+    # print (array)
+    # # mergesort(array, 0, len(array)-1)
+    # mergesort(array)
+    # print (array)
+
+def out_file(array):
+    out = open("merge3.txt", "w")
+    out.truncate(0)
+    for i in range(len(array)):
+        write = ""
+        for number in array[i]:
+            write += str(number) + " "
+        write += "\n"
+        out.write(write)
+    #print(write)
+    out.close()
+
+def mergesort3(array):
+    # if the len of the array is less than 2 return
+    if len(array) >1:
+
+        mid_lower = len(array) // 3
+        mid_upper = 2 * len(array) // 3
+        bot = array[0:mid_lower]
+        mid = array[mid_lower:mid_upper]
+        up = array[mid_upper:len(array)]
+        # print (bot)
+        # print (mid)
+        # print (up)
+        # divide, until array size is 1
+        mergesort3(bot)
+        mergesort3(mid)
+        mergesort3(up)
+        merge(array,bot,mid,up)
+
+
+def merge(array,bot,mid,up):
+    #conquer
+    i = j = k = z = 0 # bot, mid, up, main array
+    #comparing all to see what is the smallest between them
+    while i < len(bot) and j < len(mid) and k < len(up):
+        if bot[i] < mid[j]:
+            if bot[i] < up[k]:
+                array[z] = bot[i]
+                i+=1
+            else:
+                array[z] = up[k]
+                k+=1
+            z+=1
+        else: # if mid is smaller or up is smaller
+            if mid[j] < up[k]:
+                array[z] = mid[j]
+                j+=1
+            else:
+                array[z] = up[k]
+                k+=1
+            z+=1
+
+    # just like mergesort2, merge 1 and 2, 2 and 3, and 1 and 3
+    while i < len(bot) and j < len(mid):
+        if bot[i] < mid[j]:
+            array[z] = bot[i]
+            i+=1
+        else:
+            array[z] = mid[j]
+            j+=1
+        z+=1
+    while j < len(mid) and k < len(up):
+        if mid[j] < up[k]:
+            array[z] = mid[j]
+            j+=1
+        else:
+            array[z] = up[k]
+            k+=1
+        z+=1
+    while i < len(bot) and k < len(up):
+        if bot[i] < up[k]:
+            array[z] = bot[i]
+            i+=1
+        else:
+            array[z] = up[k]
+            k+=1
+        z+=1
+
+
+    #add the rest of the list back up
+    while i < len(bot):
+        array[z] = bot[i]
+        i += 1
+        z += 1
+
+    while j < len(mid):
+        array[z] = mid[j]
+        j += 1
+        z += 1
+
+    while k < len(up):
+        array[z] = up[k]
+        k += 1
+        z += 1
+
+main()
